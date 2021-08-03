75d2015446c37c3b64f2c1d1d24c0dd377df47ba
smandapakaa <subrahmanyam.mbr@outlook.com>
2021-08-03 18:31:52 -0400

    1 file modified

--- index.js
+++ index.js
@@ -1,87 +1,15 @@
-// run `node index.js` in the terminal
-const { isNil, isEmpty } = require('ramda');
-
-const test = true;
-
-const { one } = test ?? 'c';
-
-console.log('one: ', one);
-
-const tokens = [
-  {
-    key: 'value',
-    value: '1'
-  }
+const { prop, sort, sortWith, ascend, descend } = require('ramda');
+const students = [
+  { name: 'Quentin', studentBid: '1', formula: 1, efc: 0, originalNeed: 9500 },
+  { name: 'Eliot', studentBid: '2', formula: 1, efc: 500, originalNeed: 10000 },
+  { name: 'Margo', studentBid: '3', formula: 2, efc: 1500, originalNeed: 9500 },
+  { name: 'Julia', studentBid: '4', formula: 2, efc: 1000, originalNeed: 9000 },
+  { name: 'Kady', studentBid: '5', formula: 3, efc: 0, originalNeed: 1000 },
+  { name: 'Penny', studentBid: '6', formula: 4, efc: 500, originalNeed: 8000 }
 ];
 
-let releaseStatus = [
-  { value: '3', label: 'Complete' },
-  { value: '5', label: 'Correction Received' },
-  { value: '0', label: 'ISIR Not Received' },
-  { value: '4', label: 'In Process' },
-  { value: '1', label: 'Not Released' },
-  { value: '2', label: 'Released' }
-];
-releaseStatus = null;
-// [{ token: '$$value$$', value }]
-const getTokenArray = (tokens, releaseStatus) =>
-  tokens.map(token => {
-    if (!isEmpty(releaseStatus)) {
-      return (
-        releaseStatus &&
-        releaseStatus
-          .filter(status => status.value === token.value)
-          .map(ele => {
-            return {
-              token: `$$${token.key}$$`,
-              value: ele.label
-            };
-          })
-          .reduce(e => e)
-      );
-    }
-
-    return {
-      token: `$$${token.key}$$`,
-      value: token.value
-    };
-  });
-
-console.log('result: ', getTokenArray(tokens, releaseStatus));
-
-/*const getTokenArray = (tokens, releaseStatus) =>
-  tokens.map(token => {
-    return releaseStatus
-      .filter(status => status.value === token.value)
-      .map(ele => {
-        const key = `${token.key}`;
-        return {
-          token: `$$${key}$$`,
-          value: ele.label
-        };
-      })
-      .reduce(e => e);
-  });
-  */
-const getTokenArray1 = tokens =>
-  tokens.map(token => {
-    const { codeLookup = '' } = token;
-    const codeLookups =
-      Util.getOptionsForCBSelectFromCodeLookup(codeLookupMap, codeLookup) ?? [];
-
-    if (!isEmpty(codeLookups)) {
-      return codeLookups
-        .filter(status => status.value === token.value)
-        .map(ele => {
-          return {
-            token: `$$${token.key}$$`,
-            value: ele.label
-          };
-        })
-        .reduce(e => e);
-    }
-    return {
-      token: `$$${token.key}$$`,
-      value: token.value
-    };
-  });
+const sorted = sortWith(
+  [descend(prop('formula')), ascend(prop('efc'))],
+  students
+);
+console.log('sorted: ', sorted);
